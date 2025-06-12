d70bfc9fc890ab20b6771d016ece61b7694af105
eymatsuda2016 <eymatsuda2016@outlook.com>
2025-06-12 20:17:24 -0300

    Criado Modulo Categorias e Testado no Layout

--- src/app/template/template-routing.module.ts
+++ src/app/template/template-routing.module.ts
@@ -2,9 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 
+
 const routes: Routes = [
-  {path: '',
-    component: LayoutComponent
+  {
+    path: '',
+    component: LayoutComponent, 
+    children: [
+      {
+        path:'categorias',
+        loadChildren: () => import('../categorias/categorias.module').then(m => m.CategoriasModule)
+      }
+    ]
+
   }
 ];
 

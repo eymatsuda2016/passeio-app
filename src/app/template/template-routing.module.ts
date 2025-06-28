0dad588799b82387cafef9c43143603f72042591
eymatsuda2016 <eymatsuda2016@outlook.com>
2025-06-27 22:02:58 -0300

    Capturando Eventos de Rotas para Definir o Título e Subtitulo das Categorias

--- src/app/template/template-routing.module.ts
+++ src/app/template/template-routing.module.ts
@@ -10,17 +10,20 @@ const routes: Routes = [
       {
         path: 'categorias',
         loadChildren: () => import('../categorias/categorias.module').then(m => m.CategoriasModule),
-        pathMatch: 'full'
+        pathMatch: 'full',
+        data : {titulo: 'Categorias', subtitulo: 'Realize o Cadastro de Novas Categorias'}
       },
       {
         path: 'lugares',
         loadChildren: () => import('../lugares/lugares.module').then(m => m.LugaresModule),
-        pathMatch: 'full'
+        pathMatch: 'full',
+        data : {titulo: 'Lugares', subtitulo: 'Realize o Cadastro de Novos Lugares'}
       },
       {
         path: 'galeria',
         loadChildren:() => import('../galeria/galeria.module').then(m => m.GaleriaModule),
-        pathMatch: 'full'
+        pathMatch: 'full',
+        data : {titulo: 'Lista de Lugares Legais', subtitulo: 'Descubra os Melhores Lugares para Explorar e se Divertir'}
       }
     ]
   }
